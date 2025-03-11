fcb405144e108f1beddb0db9b8ca92b96e664bda
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2025-03-11 16:08:56 +0100

    SP lending ok?

---
+++ DB/StockedProcedures/SP_Lending_Insert.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[SP_Lending_Insert]
+	@game_copy_id int,
+	@owner_id uniqueidentifier,
+	@borrower_id uniqueidentifier,
+	@lending_date datetime
+AS
+Begin
+
+	Insert into [Lending]
+			([Game_Copy_id],
+			[Lending_Date],
+			[Owner_Id],
+			[Borrower_Id])
+	
+	Output [inserted].[Lending_id]
+
+	Values (@game_copy_id,
+			@lending_date,
+			@owner_id,
+			@borrower_id)
+End
