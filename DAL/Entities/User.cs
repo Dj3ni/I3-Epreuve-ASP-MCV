39f5c51cd466ebb36a5ca9e57a3196a7275bd6d9
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2025-03-12 10:28:49 +0100

    init repositories

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+	public class User
+	{
+		public string Pseudo {  get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTime? Deactivation_Date { get; set; }
+	}
+}
---
+++ DB/StockedProcedures/SP_Library_GetByUserId.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SP_Library_GetByUserId]
+	@user_id uniqueidentifier
+AS
+Begin
+	Select
+	[Game_Copy_Id],
+	[State]
+	[Game_Id]
+
+	FROM [Library]
+
+	Where [User_Id] = @user_id
+
+End
