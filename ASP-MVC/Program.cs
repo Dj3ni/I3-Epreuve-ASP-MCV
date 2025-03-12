b80c4de4ebb114e594117660f37bc132a1e68299
Jenny Fernandez Garcia <fernandezgarcia.jenny@gmail;com>
2025-03-12 12:08:25 +0100

    User injection in Program.cs

--- ASP-MVC/Program.cs
+++ ASP-MVC/Program.cs
@@ -1,3 +1,5 @@
+using Common.Repositories;
+
 namespace ASP_MVC
 {
 	public class Program
@@ -8,6 +10,11 @@ namespace ASP_MVC
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
+
+			// Personalized Services
+				//User
+	builder.Services.AddScoped<IUserRepository<DAL.Entities.User>,DAL.Services.UserService>();
+			builder.Services.AddScoped<IUserRepository<BLL.Entities.User>,BLL.Services.UserService>();
 
 			var app = builder.Build();
 
